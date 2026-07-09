5dc40657b3db276689fdedd05ed8621c5629f6d7
sinan <sinanbh9605@gmail.com>
2026-07-09 15:34:47 +0530

    updated

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,7 +10,7 @@ class HomeController extends Controller
     public function index()
     {
         $data['meta_title'] = 'Best bpo outsourcing company in UK  | Allianze Digital';
-        $data['meta_keywords'] = 'BPO outsourcing company UK, business process outsourcing services UK, outsourcing company UK, offshore BPO services, data entry outsourcing UK, virtual assistant services UK, healthcare BPO services UK, insurance outsourcing UK, image editing outsourcing services, BPO company Nottingham, affordable data entry outsourcing UK, business process outsourcing for small business UK';
+        $data['meta_keywords'] = 'Best bpo services in UK, best backoffice outsourcing in uk , best call center service in UK, case data entry, secure legal data management, Allianze Digital';
         $data['meta_description'] = 'digital marketing, SEO, PPC, social media, content marketing, web design, Allianze Digital';
         return view('home', $data);
     }
